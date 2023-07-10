56ae4b63f3cb9d2d72b8b6b0a26bf6bd1c41a24c
viviannet97 <132000560+viviannet97@users.noreply.github.com>
2023-07-10 22:20:45 +0000

    Update todos

--- src/js/component/TodoContainer.jsx
+++ src/js/component/TodoContainer.jsx
@@ -1,35 +1,36 @@
 import React, { useState } from "react";
 import Todo from "./Todo.jsx";
 
-
 export default function TodoContainer() {
-
   const [userInput, setUserInput] = useState("");
   const [todoList, setTodoList] = useState([]);
- 
 
   const onChangeHandler = (event) => {
     const { value } = event.target;
     console.log(value);
     setUserInput(event.target.value);
-    // if(todoList.length===0){
-      
-    //   place = change.placeholder = "No tasks, add a task";
-       
-      
-    
-    // }
   };
 
+ 
 
-  
+  const changeHandler = (arr) => {
+
+    if (arr.length === 0) {
+     return <Todo todo = "No tasks, add a task"/>
+    } else {
+      return todoList.map((todo, index) => (
+        <Todo key={index} index={index} todo={todo} removeTodo={removeTodo} />
+      ))
+    }
+  };
+
+ 
   const removeTodo = (key) => {
     //filter()
     // avoid mutating todoList
     // dont use .splice
     setTodoList(todoList.filter((list, index) => index !== key));
-   
-   
+
     return console.log(todoList);
   };
 
@@ -38,16 +39,12 @@ export default function TodoContainer() {
   const addTodoHandler = (event) => {
     event.preventDefault();
 
-  
-
     if (event.key === "Enter") {
       setTodoList([...todoList, userInput]);
       setUserInput("");
     }
-  
   };
- 
-  
+
   return (
     <>
       <form onSubmit={(e) => e.preventDefault()}>
@@ -57,19 +54,14 @@ export default function TodoContainer() {
           onChange={onChangeHandler}
           onKeyUp={addTodoHandler}
           required
-          placeholder= "What need to be done?"
-        
-          
+          placeholder="What need to be done?"
         />
       </form>
 
-      {todoList.map((todo, index) => <Todo key={index} index={index} todo={todo} removeTodo={removeTodo} />)}
-
-      <p id="itemsLeft" >
-        {todoList.length} items left</p>
-      
-      
+      {changeHandler(todoList)}
      
+
+      <p id="itemsLeft">{todoList.length} items left</p>
     </>
-  )
+  );
 }
